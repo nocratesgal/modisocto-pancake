23a4da19999beeb505f2888e68ac2572043623f5
nocratesgal <nocratesgal@gmail.com>
2025-10-22 10:56:29 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://modlynx.xyz',
-  output: 'static',
-  build: {
-    inlineStylesheets: 'auto'
-  }
+import { defineConfig } from 'astro/config';  
+  
+export default defineConfig({  
+  site: 'https://modlynx.xyz',  
+  output: 'static',  
+  build: {  
+    inlineStylesheets: 'auto'  
+  }  
 });
